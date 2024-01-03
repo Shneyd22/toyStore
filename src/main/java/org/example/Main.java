9dd7a9cdc09ffe82648d2d69359ef363d7e84f0e
Sergei Schneidmiller <5797221@gmail.com>
2024-01-03 12:07:53 +0300

    Toy store is ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,61 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        ArrayList<Toy> toys = new ArrayList<>();
+        ArrayDeque<Toy> prizeQueue = new ArrayDeque<>();
+        Toy bear = new Toy(1, "Мишка", 5);
+        Toy robot = new Toy(2, "Робот", 3);
+        Toy doll = new Toy(3, "Кукла", 2);
+        toys.add(bear);
+        toys.add(robot);
+        toys.add(doll);
+        prizePoolCreator(toys, prizeQueue);
+        System.out.println(prizeQueue);
+        prizeIssuanceFileCreator(prizeQueue);
+
     }
-}
+    public static void prizePoolCreator(ArrayList<Toy> toys, ArrayDeque<Toy> prizeQueue){
+        ArrayList<Toy> prizePool = new ArrayList<>();
+        for (Toy toy: toys) {
+            for (int i = 0; i < toy.quantity; i++) {
+                prizePool.add(toy);
+            }
+        }
+        Collections.shuffle(prizePool);
+        for (Toy prize: prizePool) {
+            prizeQueue.add(prize);
+        }
+    }
+
+    public static void prizeIssuanceFileCreator(ArrayDeque<Toy> prizeQueue) throws Exception{
+        File file = new File("prizeIssuance.txt");
+        while (prizeQueue.peek()!=null){
+            Toy e = prizeQueue.remove();
+            if (file.createNewFile()) {
+                FileWriter fileWriter = new FileWriter("prizeIssuance.txt", false);
+                fileWriter.write("id: " + e.id +
+                        ", name: " + e.name +
+                        ", quantity: " + e.quantity);
+                fileWriter.close();
+            } else {
+                FileWriter fileWriter = new FileWriter("prizeIssuance.txt", true);
+                fileWriter.write("\n" +
+                        "id: " + e.id +
+                        ", name: " + e.name +
+                        ", quantity: " + e.quantity);
+                fileWriter.close();
+                }
+            }
+        }
+    }
+
+
+
--- src/main/java/org/example/Toy.java
+++ src/main/java/org/example/Toy.java
@@ -1,2 +1,47 @@
-package org.example;public class Toy {
+package org.example;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Toy  {
+    public int id;
+    public String name;
+    public int quantity;
+
+    public Toy(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return quantity;
+    }
+
+    public void setWeight(int weight) {
+        if (weight >= 0 && weight <= 10) {
+            this.quantity = quantity;
+        } else throw new RuntimeException("Введено неверное значение вероятности выпадения");
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
